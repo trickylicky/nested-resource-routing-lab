01f09286e98f8324271eda4011a5c0a2576696f4
Tricky Licky <wangombevictor5@gmail.com>
2022-12-11 17:41:39 +0300

    complete assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # def index
+  #   items = Item.all
+  #   render json: items, include: :user
+  # end
 
-  def index
-    items = Item.all
-    render json: items, include: :user
+  def index 
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+      return render json: items, include: :user
+  end 
+
+
+
+  def show
+    item = Item.find_by!(id: params[:id])
+    render json: item
+  end
+
+  def create
+    item = Item.create!(item_params)
+    render json: item, status: :created
+  end
+
+  private
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
